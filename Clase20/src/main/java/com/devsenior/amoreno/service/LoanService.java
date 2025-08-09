3f989cab38bacc4d67c960f0a0ba76a0920dcb0a
Andres Moreno Pedraza <jamp9504@gmail.com>
2025-08-09 12:16:14 -0500

    backend terminado sin pruebas

---
+++ Clase20/src/main/java/com/devsenior/amoreno/service/LoanService.java
@@ -0,0 +1,37 @@
+package com.devsenior.amoreno.service;
+
+import com.devsenior.amoreno.exception.NotFoundException;
+import com.devsenior.amoreno.model.Loan;
+import com.devsenior.amoreno.model.LoanState;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoanService {
+
+    private List<Loan> loans;
+    private BookService bookService;
+    private UserService userService;
+
+    public LoanService(BookService bookService, UserService userService) {
+        this.bookService = bookService;
+        this.userService = userService;
+        this.loans = new ArrayList<>();
+    }
+
+    public void addLoan(String id, String isbn) throws NotFoundException {
+        var user = userService.getUserById(id);
+        var book = bookService.getBookByIsbn(isbn);
+        loans.add(new Loan(user, book));
+    }
+
+    public void returnBook(String id, String isbn) throws NotFoundException {
+        for (var loan : loans) {
+            if (loan.getUser().getId().equals(id) && loan.getBook().getIsbn().equals(isbn) && loan.getLoanDate().equals(LoanState.STARTED)) {
+                loan.setState(LoanState.FINISHED);
+                return;
+            }
+        }
+        throw new NotFoundException("No existe un prestamo del libro: " + isbn + " para el usuario: " + id);
+    }
+}
